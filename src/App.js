aedb025f3e9642d03e0b4dad8ac93f8f60527d32
Владислав Объедков <vladislavobedkov@MBP-Vladislav.Dlink>
2023-02-27 23:42:15 +0600

    Fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./widgets/Header/Header";
+import Screamer from "./widgets/Screamer";
 
 function App() {
-  return <Header />;
+  const [isScreamer, setIsScreamer] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsScreamer(true), 5000);
+  }, []);
+  return (
+    <div className="App">
+      <Header />
+      {isScreamer && <Screamer />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/widgets/Screamer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Screamer = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Бууууууууу!!!!!</h1>
+    </div>
+  );
+};
+
+export default Screamer;
